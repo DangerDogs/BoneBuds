d5603c5b006bfcc0eaa9dc0c13ef56f9655ef35b
David Darley <daviddar232@gmail.com>
2018-01-09 15:25:14 -0800

    [update] - adding a userpage that connects to the edit profile page.

--- client/src/Content/ContentContainer.jsx
+++ client/src/Content/ContentContainer.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MapContainer from './Routes/MapContainer';
 import UserProfile from './Routes/UserProfile';
+import UserPage from './Routes/UserPage';
 
 const ContentContainer = (props) => (
   <main>
     <Switch>
       <Route exact path='/' component={MapContainer} />
+      <Route path='/userpage' render={ () => (<UserPage currentUser={props.currentUser}/>) } />
       <Route path='/userprofile' render={ () => (<UserProfile currentUser={props.currentUser}/>) } />
     </Switch>
   </main>
